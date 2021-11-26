8f96530ca227d6b47f4ee347bd9a168204f764c1
sthadevraj <95071871+sthadevraj@users.noreply.github.com>
2021-11-26 23:00:15 +0545

    Add files via upload

---
+++ group.js
@@ -0,0 +1,9 @@
+import Groups from "../components/Groups";
+import ContainerBlock from "../components/ContainerBlock";
+export default function Group() {
+  return (
+    <ContainerBlock>
+      <Groups />
+    </ContainerBlock>
+  );
+}
